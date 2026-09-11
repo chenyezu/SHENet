d6eab30e3e22f2957f4d5f015f55be12ae439dd8
cyz2 <165636856+chenyezu@users.noreply.github.com>
2026-09-11 21:47:52 +0800

    Update train.py

--- train.py
+++ train.py
@@ -255,7 +255,6 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=50)
     parser.add_argument('--bs_reduct', type=int, default=5)
     parser.add_argument('--workers', type=int, default=6)
-    parser.add_argument('--m', type=int, default=40)
     parser.add_argument('--topk', type=int, default=8)
     parser.add_argument('--warmup', type=int, default=10000)
     parser.add_argument('--lr_xe', type=float, default=1e-4)
